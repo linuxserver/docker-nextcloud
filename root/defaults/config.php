91f7977effbfd5a0740856c985879b093933c36f
Eric Nemchik <eric@nemchik.com>
2023-06-06 23:00:47 -0500

    setup config
    Signed-off-by: Eric Nemchik <eric@nemchik.com>

--- root/defaults/config.php
+++ root/defaults/config.php
@@ -1,6 +1,18 @@
 <?php
-$CONFIG = array (
-  'memcache.local' => '\OC\Memcache\APCu',
+$CONFIG = [
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'memcache.distributed' => '\\OC\\Memcache\\Memcached',
   'datadirectory' => '/data',
-  );
-
+  'apps_paths' => [
+    0 => [
+      'path' => '/app/www/public/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ],
+    1 => [
+      'path' => '/app/www/public/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ],
+  ],
+];
